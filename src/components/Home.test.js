f062fb46693d1c88b719f12472d3f87c99f2f99b
Ayyappag15 <ayyappag15@gmail.com>
2025-07-10 21:02:11 +0530

    Rempoved unwanted code

--- src/components/Home.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import Home from './Home';
-
-test('calls setIsAuthenticated(false) when logout is clicked', () => {
-  const mockLogout = jest.fn();
-  render(<Home setIsAuthenticated={mockLogout} />);
-  fireEvent.click(screen.getByText(/Logout/i));
-  expect(mockLogout).toHaveBeenCalledWith(false);
-});
